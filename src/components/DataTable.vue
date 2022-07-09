0cc3a43aa0f416e16ddcb6ecaf82d4f1fe64600f
Naguib <naguib.mohamed1999@gmail.com>
2022-07-10 01:37:51 +0200

    get temperature per department and station

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -1,6 +1,7 @@
 <template>
-  <v-container>
-    <v-card>
+  <v-container fluid class="pa-md-10 "
+  >
+    <v-card class="elevation-2 mt-2 rounded-0">
       <v-alert
           dense
           outlined
@@ -9,14 +10,7 @@
       >
         La profondeur d'accès aux résultats est  <strong>: 20000</strong>
       </v-alert>
-      <v-card-title>
-        <v-text-field
-            v-model="search"
-            label="Search"
-            single-line
-            hide-details
-        ></v-text-field>
-      </v-card-title>
+
 
       <v-data-table
           :page="page"
@@ -26,6 +20,8 @@
           :options.sync="options"
           :server-items-length="totalTemperatures"
           :loading="loading"
+          :search="filterTemp"
+          item-key="resultat"
           :footer-props="{
           showFirstLastPage: true,
            'items-per-page-options': [5, 15, 30, 50, 100, 5000, -1],
@@ -37,12 +33,55 @@
           }"
          >
 
-<!--        <template v-slot:[`footer.page-text`]>-->
-<!--          <v-pagination-->
-<!--              v-model="page"-->
-<!--              :length="pageCount">-->
-<!--          </v-pagination>-->
-<!--        </template>-->
+        <template v-slot:[`item.uri_station`]="{ item }">
+          <a :href="item.uri_station">{{ item.uri_station }}</a>
+        </template>
+
+        <template v-slot:[`item.uri_cours_eau`]="{ item }">
+          <a :href="item.uri_cours_eau">{{ item.uri_cours_eau }}</a>
+        </template>
+
+        <template v-slot:top>
+          <v-row class="d-flex">
+            <v-col cols="3" class="mr-auto">
+              <v-text-field
+                  v-model="filterTemp"
+                  label="Rechercher par département"
+                  class="mx-4"
+                  solo
+              ></v-text-field>
+            </v-col>
+
+            <v-col cols="2" >
+              <v-select
+                  :items="itemsDepart"
+                  label="Code département"
+                  solo
+                  v-model="codeDepart"
+              ></v-select>
+            </v-col>
+
+            <v-col cols="2" >
+              <v-select
+                  :items="itemsStation"
+                  label="Code station"
+                  solo
+                  v-model="codeStation"
+              ></v-select>
+            </v-col>
+
+            <v-col cols="1" >
+              <v-select
+                  :items="items"
+                  label="Order"
+                  solo
+                  v-model="toggleSort"
+              ></v-select>
+            </v-col>
+          </v-row>
+
+
+        </template>
       </v-data-table>
     </v-card>
   </v-container>
@@ -54,14 +93,28 @@ import axios from "axios";
 import {ref} from "vue";
 
 let profondeur  = ref(0)
+// let currentPage = ref(1)
 
 export default {
   name: "DataTable",
   data() {
     return {
+      toggleSort: 'asc',
+      items: ['asc', 'desc'],
+      itemsDepart: [
+          45,
+          33
+      ],
+      itemsStation : [],
+      codeStation: '',
+      // sortBy: "resultat",
+      // sortAsc: false,
       errorProfondeur: false,
-      search: '',
+      filterTemp: '',
       page: 1,
+      codeDepart: '',
+      totalStations: 0,
+      stations: [],
       totalTemperatures: 0,
       numberOfPages: 0,
       temperatures: [],
@@ -69,11 +122,27 @@ export default {
       options: {},
       headers: [
         { text: "Code Station", value: "code_station"},
-        { text: "Date mesure", value: "date_mesure_temp"},
-        { text: "heure_mesure_temp", value: "heure_mesure_temp"},
-        { text: "resultat", value: "resultat", sortable: false  },
-        { text: "code_unite", value: "code_unite", sortable: false  },
-        { text: "symbole_unite", value: "symbole_unite", sortable: false  }
+        { text: "libelle_station", value: "libelle_station", sortable: false  },
+        { text: "uri_station", value: "uri_station", sortable: false  },
+        { text: "longitude", value: "longitude", sortable: false  },
+        { text: "latitude", value: "latitude", sortable: false  },
+        { text: "code commune", value: "code_commune", sortable: false  },
+        { text: "libelle_commune", value: "libelle_commune", sortable: false  },
+        { text: "code_cours_eau", value: "code_cours_eau", sortable: false  },
+
+        { text: "uri_cours_eau", value: "uri_cours_eau", sortable: false  },
+        { text: "code_parametre", value: "code_parametre", sortable: false  },
+        { text: "libelle_parametre", value: "libelle_parametre", sortable: false  },
+
+        { text: "Date mesure temp", value: "date_mesure_temp"},
+        { text: "Heure mesure temp", value: "heure_mesure_temp"},
+        { text: "Resultat", value: 'resultat' },
+        { text: "Code unite", value: "code_unite", sortable: false  },
+        { text: "Symbole unite", value: "symbole_unite", sortable: false  },
+
+        { text: "code_qualification", value: "code_qualification", sortable: false  },
+        { text: "libelle_qualification", value: "libelle_qualification", sortable: false  },
+
       ],
     };
   },
@@ -91,9 +160,40 @@ export default {
       deep: true,
     },
 
+    toggleSort: function() {
+      this.readDataFromAPI();
+    },
+
+    codeDepart: function (){
+      this.readDataStationsFromAPI();
+    },
+
+    codeStation: function() {
+      this.readDataFromAPI();
+    }
+
+
+
+
   },
   methods: {
     //Reading data from API method.
+    readDataStationsFromAPI() {
+      console.log(this.codeDepart)
+      axios
+          .get(
+              "https://hubeau.eaufrance.fr/api/v1/temperature/station?code_departement="+this.codeDepart+"&size=20&exact_count=true"
+          )
+          .then((response) => {
+            this.stations = response.data.data;
+            console.log(this.stations)
+            let st;
+            for (st in this.stations) {
+              this.itemsStation.push(this.stations[st].code_station);
+            }
+            this.totalStations = response.data.count;
+          });
+    },
     readDataFromAPI() {
       this.loading = true;
       const { page, itemsPerPage } = this.options;
@@ -112,17 +212,19 @@ export default {
       console.log("prof",profondeur.value)
 
       if (profondeur.value <= 20000) {
+        console.log("entrée")
         axios
             .get(
-                "https://hubeau.eaufrance.fr/api/v1/temperature/chronique?size="+itemsPerPage
+                "https://hubeau.eaufrance.fr/api/v1/temperature/chronique?code_station="+this.codeStation+"&size="+itemsPerPage+"&sort="+this.toggleSort
                 +"&page="+ pageNumber
             )
             .then((response) => {
               this.loading = false;
               this.temperatures = response.data.data;
+              console.log(this.temperatures)
               this.totalTemperatures = response.data.count;
               this.numberOfPages = this.totalTemperatures / itemsPerPage;
-              // this.passengers = response.data.data.map((item) => {
+              // this.temperatures = response.data.data.map((item) => {
               //   return {
               //     details: {},
               //     ...item
@@ -140,6 +242,36 @@ export default {
   mounted() {
     this.readDataFromAPI();
   },
+
+  computed: {
+    // filterTemp() {
+    //   console.log("salut")
+    //   return null
+    // }
+    // temperatures(){
+    //   if (this.filterTemp){
+    //     return this.temperatures.filter(temp =>
+    //     temp.value.toLowerCase().includes(this.filterTemp.toLowerCase())
+    //     )
+    //   }
+    // }
+  }
+
+      // headers() {
+    //   return [
+    //     {
+    //       text: 'Code Station',
+    //       align: 'left',
+    //
+    //       value: 'code_station',
+    //     },
+    //     { text: "Date mesure", value: "date_mesure_temp"},
+    //     { text: "Heure mesure temp", value: "heure_mesure_temp"},
+    //     { text: "Resultat", value: "resultat",  filter: this.nameFilter, sortable: false, },
+    //     { text: "Code unite", value: "code_unite", sortable: false  },
+    //     { text: "Symbole unite", value: "symbole_unite", sortable: false  }
+    //   ]
+
 }
 </script>
 
