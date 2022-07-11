b1e2e04151637d38d29cc3efedc864445af5e133
Naguib <naguib.mohamed1999@gmail.com>
2022-07-11 02:28:17 +0200

    fixed css

--- src/views/ChartView.vue
+++ src/views/ChartView.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <v-container fluid class="pa-md-8">
-      <div class="mb-8 pa-md-5 justify-center blue">
+      <div class="mb-8 pa-md-5 justify-center blue rounded">
         <h2 class="white--text mb-6">
-          Les grahiques ci-dessous pour permet de consulter les variations des
-          températures d'un cours d'eau sur une période sélectionée.
+          Les variations des températures d'un cours d'eau sur une période
+          sélectionée.
         </h2>
 
         <h4 style="font-size: 18px">Veuillez selectionner une période.</h4>
